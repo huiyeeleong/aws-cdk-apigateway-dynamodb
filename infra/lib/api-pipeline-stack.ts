4d7e2ad6a71ed8c2adce5207e28ac4d763041d6f
Leong <hui.yee.leong@accenture.com>
2022-08-19 13:14:57 +1000

    Updated Role

--- infra/lib/api-pipeline-stack.ts
+++ infra/lib/api-pipeline-stack.ts
@@ -6,25 +6,25 @@ import {
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import * as iam from "aws-cdk-lib/aws-iam"
 import { MyPipelineAppStage } from "./api-stack-stage";
-import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { ManagedPolicy, Role} from 'aws-cdk-lib/aws-iam';
 
 export class ApiPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        //create role for codepipeline
-        const actionRole = new iam.Role(this, 'ActionRole', {
-            assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com'),
-            description: 'CodePipeline Role',
-            managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')]
-          });
+        // //create role for codepipeline //for testing purpose only // best practice not too add admin permission
+        // const actionRole = new iam.Role(this, 'ActionRole', {
+        //     assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com'),
+        //     description: 'CodePipeline Role',
+        //     managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')]
+        //   });
         
 
         //connect code star arn with github
         //const connectionArn = ssm.StringParameter.valueForStringParameter(this, '/serverless-api/git/connection-arn', 1);
         const buildCommands = [
-            'npm ci',
-            'npm run build',
+            //'npm ci',
+            //'npm run build',
             'cd infra',
             'npm ci',
             'npm run build',
@@ -35,15 +35,18 @@ export class ApiPipelineStack extends Stack {
         //create codepipeline
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'ServerlessAPI-Pipeline',
+            
             synth: new ShellStep('Build', {
                 input: CodePipelineSource.connection('huiyeeleong/aws-cdk-apigateway-dynamodb', 'main', {
                     connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:951639499020:connection/281450c5-d4ba-4650-9145-4f4b9cf7c6dc',
+                    
                 }),
                 commands: buildCommands
+                
             }),
             selfMutation: true,
         });
-
         pipeline.addStage(new MyPipelineAppStage(this, "Deploy"));
+        
     }
 }
